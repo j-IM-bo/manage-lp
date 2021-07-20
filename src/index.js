17de2a91edeeac56a5ebe1f0b256880aed582376
j-IM-bo <75038061+j-IM-bo@users.noreply.github.com>
2021-07-20 21:24:16 +0100

    setup

---
+++ src/index.js
@@ -0,0 +1,11 @@
+//css
+import "./sass/style.scss";
+
+//js
+// import { debounce } from "./js/utilities/debounce";
+// import { animateOnScroll } from "./js/utilities/aos";
+// import { menu } from "./js/menu";
+import { header } from "./js/header";
+
+console.log("hello");
+header();
---
+++ src/js/utilities/aos.js
@@ -0,0 +1,16 @@
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+export function animateOnScroll() {
+    AOS.init({
+        offset: -100,
+        duration: 600,
+        easing: "ease-in-out",
+        once: true,
+        delay: 250,
+    });
+
+    window.addEventListener("resize", () => {
+        AOS.refresh();
+    });
+}
