d75176f8d9472db2e1e2bb33d1ed2084856bfe32
j-IM-bo <75038061+j-IM-bo@users.noreply.github.com>
2021-07-23 15:13:38 +0100

    features section

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import "./sass/style.scss";
 
 //js
 import { debounce } from "./js/utilities/debounce";
-// import { animateOnScroll } from "./js/utilities/aos";
+import { animateOnScroll } from "./js/utilities/aos";
 import { menu } from "./js/menu";
 import { header } from "./js/header";
 
 function init() {
     menu();
     window.addEventListener("scroll", debounce(header), false);
+    animateOnScroll();
 }
 
 window.addEventListener("load", function () {
--- src/js/utilities/aos.js
+++ src/js/utilities/aos.js
@@ -1,13 +1,13 @@
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export function animateOnScroll() {
     AOS.init({
         offset: -100,
-        duration: 600,
+        duration: 900,
         easing: "ease-in-out",
         once: true,
-        delay: 250,
+        delay: 250
     });
 
     window.addEventListener("resize", () => {
