4249d03077763694c29bf54e84e8787b1be9d0bc
j-IM-bo <75038061+j-IM-bo@users.noreply.github.com>
2021-07-26 19:38:08 +0100

    footer & form

--- src/index.js
+++ src/index.js
@@ -4,15 +4,19 @@ import "./sass/style.scss";
 //js
 import { debounce } from "./js/utilities/debounce";
 import { animateOnScroll } from "./js/utilities/aos";
-import { carousel } from "./js/utilities/carousel";
+import { carousel } from "./js/carousel";
 import { menu } from "./js/menu";
 import { header } from "./js/header";
+import { validateEmail } from "./js/validateEmail";
+
+validateEmail();
 
 function init() {
     menu();
     window.addEventListener("scroll", debounce(header), false);
     animateOnScroll();
     carousel();
+    validateEmail();
 }
 
 window.addEventListener("load", function () {
